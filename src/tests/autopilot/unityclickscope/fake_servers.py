d05d077f5bc9360270aa9213509920f3dbf6c6e6
Leo Arias <leo.arias@canonical.com>
2014-01-02 23:22:14 -0600

    Removed the threading mixin.

--- src/tests/autopilot/unityclickscope/fake_servers.py
+++ src/tests/autopilot/unityclickscope/fake_servers.py
@@ -1,6 +1,6 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 #
-# Copyright (C) 2013 Canonical Ltd.
+# Copyright (C) 2013, 2014 Canonical Ltd.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3, as published
@@ -15,17 +15,10 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import BaseHTTPServer
-import SocketServer
 import urlparse
 
 
-class MultiThreadedHTTPServer(SocketServer.ThreadingMixIn,
-                              BaseHTTPServer.HTTPServer,
-                              object):
-    pass
-
-
-class FakeSearchServer(MultiThreadedHTTPServer):
+class FakeSearchServer(BaseHTTPServer.HTTPServer, object):
 
     def __init__(self, server_address):
         super(FakeSearchServer, self).__init__(
