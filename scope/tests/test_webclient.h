d30a2299dc946bbcda02b262d4299aa8af97b06c
alecu <alecu@canonical.com>
2014-01-21 20:07:49 -0300

    fixes requested on cmake review

--- scope/tests/test_webclient.h
+++ scope/tests/test_webclient.h
@@ -35,7 +35,6 @@
 
 const QString FAKE_SERVER = "http://fake-server/";
 const QString FAKE_PATH = "fake/api/path";
-const QString FAKE_JSON_SEARCH_RESULT = " [ { \"name\": \"org.example.awesomelauncher\", \"title\": \"Awesome Launcher\", \"description\": \"This is an awesome launcher.\", \"price\": 1.99, \"icon_url\": \"http://software-center.ubuntu.com/site_media/appmedia/2012/09/SPAZ.png\", \"resource_url\": \"http://search.apps.ubuntu.com/api/v1/package/org.example.awesomelauncher\" }, { \"name\": \"org.example.fantastiqueapp\", \"title\": \"Fantastic App\", \"description\": \"This is a fantasticc app.\", \"price\": 0.0, \"icon_url\": \"http://assets.ubuntu.com/sites/ubuntu/504/u/img/ubuntu/features/icon-find-more-apps-64x64.png\", \"resource_url\": \"http://search.apps.ubuntu.com/api/v1/package/org.example.fantasticapp\" }, { \"name\": \"org.example.awesomewidget\", \"title\": \"Awesome Widget\", \"description\": \"This is an awesome widget.\", \"price\": 1.99, \"icon_url\": \"http://assets.ubuntu.com/sites/ubuntu/504/u/img/ubuntu/features/icon-photos-and-videos-64x64.png\", \"resource_url\": \"http://search.apps.ubuntu.com/api/v1/package/org.example.awesomewidget\" } ] ";
 
 class TestWebClient : public QObject
 {
