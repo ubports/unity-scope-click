51b4d91f040afa6c4a73303c30e8cc5f8f4a01cf
Diego Sarmentero <diego.sarmentero@gmail.com>
2014-02-13 18:38:12 -0300

    removing qdebus messages for testing

--- scope/click/scope.cpp
+++ scope/click/scope.cpp
@@ -86,14 +86,12 @@ scopes::QueryBase::UPtr click::Scope::create_query(unity::scopes::Query const& q
 
 unity::scopes::QueryBase::UPtr click::Scope::preview(const unity::scopes::Result& result,
         const unity::scopes::ActionMetadata&) {
-    qDebug() << "\n\nPREVIEWWWWWWWWWWWWW";
     scopes::QueryBase::UPtr previewResult(new Preview(result.uri(), index, result));
     return previewResult;
 }
 
 unity::scopes::ActivationBase::UPtr click::Scope::perform_action(unity::scopes::Result const& /*result*/, unity::scopes::ActionMetadata const& /* metadata */, std::string const& /* widget_id */, std::string const& /* action_id */)
 {
-    qDebug() << "\n\nYEAHHHHHHHHHHHHHHHHHHHHH2222222222";
     return scopes::ActivationBase::UPtr(new MyActivation());
 }
 
