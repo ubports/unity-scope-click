d521e45c2e6c650b9d7f1167faeb3927ec6e257f
Rodney Dawes <rodney.dawes@canonical.com>
2014-03-24 15:18:13 -0400

    Drop the old vala-based scope.

--- test.sh
+++
@@ -1,4 +0,0 @@
-pkill -9 click
-sleep 1
-G_MESSAGES_DEBUG=all ./src/click-scope &
-libunity-tool -n com.canonical.Unity.Scope.Applications.Click -p /com/canonical/unity/scope/applications/click -q sample -r -g
