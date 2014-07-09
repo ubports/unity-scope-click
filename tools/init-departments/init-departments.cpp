72a450c7ba26d17561e28818dc2d314703cbe90c
Pawel Stolowski <pawel.stolowski@canonical.com>
2014-07-09 17:11:25 +0200

    Support multiple locales at a time.

--- tools/init-departments/init-departments.cpp
+++ tools/init-departments/init-departments.cpp
@@ -63,20 +63,17 @@ void noDebug(QtMsgType, const QMessageLogContext&, const QString&) {}
 
 int main(int argc, char **argv)
 {
-    if (getenv("INIT_DEPARTMENTS_DEBUG") == nullptr)
+    if (argc < 3)
     {
-        qInstallMessageHandler(noDebug);
-    }
-
-    if (argc != 3)
-    {
-        std::cerr << "Usage: " << argv[0] << " DBFILE LOCALE" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " DBFILE LOCALE1 [LOCALE2 ...]" << std::endl;
         return DEPTS_ERROR_ARG;
     }
 
-    std::atomic<int> return_val(0);
     const std::string dbfile(argv[1]);
-    const std::string locale(argv[2]);
+    const std::vector<std::string> locales(argv + 2, argv + argc);
+
+    if (!getenv("INIT_DEPARTMENTS_DEBUG"))
+        qInstallMessageHandler(noDebug);
 
     std::unique_ptr<click::DepartmentsDb> db;
     try
@@ -92,19 +89,72 @@ int main(int argc, char **argv)
     auto nam = QSharedPointer<click::network::AccessManager>(new click::network::AccessManager());
     auto client = QSharedPointer<click::web::Client>(new click::web::Client(nam));
     click::Index index(client);
-    click::web::Cancellable cnc;
-    std::vector<click::web::Cancellable> cnc2;
+    std::vector<click::web::Cancellable> cnc;
 
+    std::promise<void> qt_ready;
+    std::promise<void> bootstrap_ready;
     std::promise<click::PackageSet> pkgs_ready;
+    auto qt_ready_ft = qt_ready.get_future();
+    auto bootstrap_ft = bootstrap_ready.get_future();
     auto pkgs_ft = pkgs_ready.get_future();
 
+    std::atomic<int> return_val(0);
+    std::atomic<std::vector<std::string>::size_type> num_of_locales(locales.size());
     std::atomic<click::PackageSet::size_type> num_of_pkgs(0);
 
     //
-    // a thread that does bootstrap request and loops over all packages and performs details requests
-    // it initially blocks on pkgs_ft future (waits until main thread gets all the click pcackages)
+    // a thread that does bootstrap request
     std::thread net_thread([&]() {
+        qt_ready_ft.get();
+
+        for (auto const locale: locales)
+        {
+            qt::core::world::enter_with_task([&return_val, &bootstrap_ready, &index, &cnc, &num_of_locales, &db, locale]() {
+
+                std::cout << "Getting departments for locale '" << locale << "'" << std::endl;
+                cnc.push_back(index.bootstrap([&return_val, &bootstrap_ready, &num_of_locales, &db, locale](const click::DepartmentList& depts, const click::HighlightList&, click::Index::Error error, int) {
+                    std::cout << "Bootstrap call for locale '" << locale << "' finished" << std::endl;
+
+                    if (error == click::Index::Error::NoError)
+                    {
+                        try
+                        {
+                            db->store_departments(depts, locale);
+                            std::cout << "Stored departments for locale '" << locale << "'" << std::endl;
+                        }
+                        catch (const std::exception& e)
+                        {
+                            std::cerr << "Failed to update departments database: " << e.what() << std::endl;
+                            return_val = DEPTS_ERROR_DB;
+                        }
+
+                        if (--num_of_locales == 0)
+                        {
+                            std::cout << "All locales processed" << std::endl;
+                            bootstrap_ready.set_value();
+                        }
+                    }
+                    else
+                    {
+                        std::cerr << "Network error" << std::endl;
+                        return_val = DEPTS_ERROR_NETWORK;
+                        bootstrap_ready.set_value();
+                        qt::core::world::destroy();
+                        return;
+                    }
+                }));
+            });
+        }
+    });
+
+    //
+    // a thread that iterates over all packages and performs details requests
+    // it initially blocks on bootstrap_ft and pkgs_ft future
+    // (waits until bootstrap finished and main thread gets all the click packages)
+    std::thread details_thread([&]() {
+        bootstrap_ft.get();
         auto const pkgs = pkgs_ft.get();
+
         num_of_pkgs = pkgs.size();
 
         // if click list failed or nothing to do, then end this thread and stop Qt bridge
@@ -115,83 +165,55 @@ int main(int argc, char **argv)
             return;
         }
 
-        qt::core::world::enter_with_task([&return_val, &pkgs_ft, pkgs, &index, &cnc, &cnc2, &num_of_pkgs, &db, locale]() {
+            std::cout << "Getting package details for " << num_of_pkgs << " packages" << std::endl;
 
-            std::cout << "Getting departments for locale " << locale << std::endl;
-            cnc = index.bootstrap([&return_val, &pkgs_ft, pkgs, &cnc2, &index, &num_of_pkgs, &db, locale](const click::DepartmentList& depts, const click::HighlightList&, click::Index::Error error, int) {
-                std::cout << "Bootstrap call finished" << std::endl;
+            //
+            // note: this queues requests for all the packages; the number of packages to process
+            // is kept in num_of_pkgs which gets decreased, the last processed package will stop Qt bridge.
+            for (auto const& pkg: pkgs)
+            {
+                auto const pkgname = pkg.name;
 
-                if (error == click::Index::Error::NoError)
-                {
-                    try
-                    {
-                        db->store_departments(depts, locale);
-                        std::cout << "Stored " << db->department_name_count() << " departments" << std::endl;
-                    }
-                    catch (const std::exception& e)
-                    {
-                        std::cerr << "Failed to update departments database: " << e.what() << std::endl;
-                        return_val = DEPTS_ERROR_DB;
-                    }
-                }
-                else
-                {
-                    std::cerr << "Network error" << std::endl;
-                    return_val = DEPTS_ERROR_NETWORK;
-                    qt::core::world::destroy();
-                }
+                qt::core::world::enter_with_task([&return_val, &index, &cnc, &num_of_pkgs, pkgname, &db]() {
 
-                std::cout << "Getting package details for " << num_of_pkgs << " packages" << std::endl;
+                    cnc.push_back(index.get_details(pkgname, [&return_val, &num_of_pkgs, pkgname, &db](const click::PackageDetails& details, click::Index::Error error) {
+                        std::cout << "Details call for " << pkgname << " finished" << std::endl;
 
-                //
-                // note: this queues requests for all the packages; the number of packages to process
-                // is kept in num_of_pkgs which gets decreased, the last processed package will stop Qt bridge.
-                for (auto const& pkg: pkgs)
-                {
-                    auto const pkgname = pkg.name;
-
-                    qt::core::world::enter_with_task([&return_val, &index, &cnc2, &num_of_pkgs, pkgname, &db]() {
-
-                        cnc2.push_back(index.get_details(pkgname, [&return_val, &num_of_pkgs, pkgname, &db](const click::PackageDetails& details, click::Index::Error error) {
-                            std::cout << "Details call for " << pkgname << " finished" << std::endl;
-
-                            if (error == click::Index::Error::NoError)
+                        if (error == click::Index::Error::NoError)
+                        {
+                            try
                             {
-                                try
-                                {
-                                    std::cout << "Storing package department for " << pkgname << ", " << details.department << std::endl;
-                                    db->store_package_mapping(pkgname, details.department);
-                                }
-                                catch (const std::exception& e)
-                                {
-                                    std::cerr << "Failed to update departments database: " << e.what() << std::endl;
-                                    return_val = DEPTS_ERROR_DB;
-                                }
+                                std::cout << "Storing package department for " << pkgname << ", " << details.department << std::endl;
+                                db->store_package_mapping(pkgname, details.department);
                             }
-                            else
+                            catch (const std::exception& e)
                             {
-                                std::cerr << "Network error" << std::endl;
-                                return_val = DEPTS_ERROR_NETWORK;
+                                std::cerr << "Failed to update departments database: " << e.what() << std::endl;
+                                return_val = DEPTS_ERROR_DB;
                             }
-                            if (--num_of_pkgs == 0)
-                            {
-                                std::cout << "All packages processed" << std::endl;
-                                qt::core::world::destroy();
-                            }
-                        }));
-                    });
-                }
-            });
-        });
+                        }
+                        else
+                        {
+                            std::cerr << "Network error" << std::endl;
+                            return_val = DEPTS_ERROR_NETWORK;
+                        }
+                        if (--num_of_pkgs == 0)
+                        {
+                            std::cout << "All packages processed" << std::endl;
+                            qt::core::world::destroy();
+                        }
+                    }));
+                });
+            }
     });
 
     //
     // enter Qt world; this blocks until qt::core:;world::destroy() gets called
-    qt::core::world::build_and_run(argc, argv, [&pkgs_ready, &return_val,&index]() {
+    qt::core::world::build_and_run(argc, argv, [&pkgs_ready, &qt_ready, &return_val]() {
 
-        qt::core::world::enter_with_task([&return_val, &index, &pkgs_ready]() {
+        qt::core::world::enter_with_task([&return_val, &pkgs_ready, &qt_ready]() {
             std::cout << "Querying click for installed packages" << std::endl;
-            iface.get_installed_packages([&return_val, &pkgs_ready](click::PackageSet pkgs, click::InterfaceError error) {
+            iface.get_installed_packages([&return_val, &pkgs_ready, &qt_ready](click::PackageSet pkgs, click::InterfaceError error) {
                 if (error == click::InterfaceError::NoError)
                 {
                     std::cout << "Found: " << pkgs.size() << " click packages" << std::endl;
@@ -214,12 +236,14 @@ int main(int argc, char **argv)
                         return_val = DEPTS_ERROR_CLICK_UNKNOWN;
                     }
                 }
+                qt_ready.set_value();
                 pkgs_ready.set_value(pkgs); // this unblocks net_thread
             });
         });
     });
 
     net_thread.join();
+    details_thread.join();
 
     return return_val;
 }
