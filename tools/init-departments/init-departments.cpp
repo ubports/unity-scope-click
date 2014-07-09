b8bfbef556dc0db2d72889fa427da37a20c20623
Pawel Stolowski <pawel.stolowski@canonical.com>
2014-07-09 12:07:41 +0200

    More refactoring.

--- tools/init-departments/init-departments.cpp
+++ tools/init-departments/init-departments.cpp
@@ -56,8 +56,8 @@ enum
     DEPTS_ERROR_CLICK_UNKNOWN = 12
 };
 
-static QSharedPointer<click::KeyFileLocator> keyFileLocator(new click::KeyFileLocator());
-static click::Interface iface(keyFileLocator);
+QSharedPointer<click::KeyFileLocator> keyFileLocator(new click::KeyFileLocator());
+click::Interface iface(keyFileLocator);
 
 void noDebug(QtMsgType, const QMessageLogContext&, const QString&) {}
 
@@ -94,23 +94,23 @@ int main(int argc, char **argv)
 
         qt::core::world::enter_with_task([&return_val, &bootstrap_ready, &index, &cnc, dbfile, locale]() {
 
-        std::cout << "Getting departments for locale " << locale << std::endl;
-        cnc = index.bootstrap([&return_val, &bootstrap_ready, dbfile, locale](const click::DepartmentList& depts, const click::HighlightList&, click::Index::Error error, int) {
-            std::cout << "Bootstrap call finished" << std::endl;
+            std::cout << "Getting departments for locale " << locale << std::endl;
+            cnc = index.bootstrap([&return_val, &bootstrap_ready, dbfile, locale](const click::DepartmentList& depts, const click::HighlightList&, click::Index::Error error, int) {
+                std::cout << "Bootstrap call finished" << std::endl;
 
-            if (error == click::Index::Error::NoError)
-            {
-                try
+                if (error == click::Index::Error::NoError)
                 {
-                    std::cout << "Storing departments in " << dbfile << " database" << std::endl;
-                    click::DepartmentsDb db(dbfile);
-                    db.store_departments(depts, locale);
-                    std::cout << "Stored " << db.department_name_count() << " departments" << std::endl;
-                }
-                catch (const std::exception& e)
-                {
-                    std::cerr << "Failed to update departments database: " << e.what() << std::endl;
-                    return_val = DEPTS_ERROR_DB;
+                    try
+                    {
+                        std::cout << "Storing departments in " << dbfile << " database" << std::endl;
+                        click::DepartmentsDb db(dbfile);
+                        db.store_departments(depts, locale);
+                        std::cout << "Stored " << db.department_name_count() << " departments" << std::endl;
+                    }
+                    catch (const std::exception& e)
+                    {
+                        std::cerr << "Failed to update departments database: " << e.what() << std::endl;
+                        return_val = DEPTS_ERROR_DB;
                     }
                 }
                 else
@@ -120,53 +120,46 @@ int main(int argc, char **argv)
                 }
 
                 bootstrap_ready.set_value();
+                qt::core::world::destroy();
             });
         });
-   });
-
-    std::thread thr2([&]() {
-        bootstrap_ft.get();
-
-        auto qq = qt::core::world::enter_with_task([&return_val, &index, &cnc]() {
-            std::cout << "Querying click for installed packages" << std::endl;
-            iface.get_installed_packages([&return_val](click::PackageSet pkgs, click::InterfaceError error) {
-                if (error == click::InterfaceError::NoError)
-                {
-                    std::cout << "Found: " << pkgs.size() << " click packages" << std::endl;
-                }
-                else
-                {
-                    if (error == click::InterfaceError::ParseError)
-                    {
-                        std::cerr << "Error parsing click output" << std::endl;
-                        return_val = DEPTS_ERROR_CLICK_PARSE;
-                    }
-                    else if (error == click::InterfaceError::CallError)
-                    {
-                        std::cerr << "Error calling click command" << std::endl;
-                        return_val = DEPTS_ERROR_CLICK_CALL;
-                    }
-                    else
-                    {
-                        std::cerr << "An unknown click error occured" << std::endl;
-                        return_val = DEPTS_ERROR_CLICK_UNKNOWN;
-                    }
-
-                    qt::core::world::destroy();
-                }
-            });
-        });
-        qq.get();
     });
 
     //
     // enter Qt world; this blocks until qt::core:;world::destroy() gets called
     qt::core::world::build_and_run(argc, argv, [&qt_ready, &return_val,&index, &cnc, dbfile, locale]() {
+
+        qt::core::world::enter_with_task([&return_val, &index, &cnc, &qt_ready]() {
+        std::cout << "Querying click for installed packages" << std::endl;
+        iface.get_installed_packages([&return_val, &qt_ready](click::PackageSet pkgs, click::InterfaceError error) {
+            if (error == click::InterfaceError::NoError)
+            {
+                std::cout << "Found: " << pkgs.size() << " click packages" << std::endl;
+            }
+            else
+            {
+                if (error == click::InterfaceError::ParseError)
+                {
+                    std::cerr << "Error parsing click output" << std::endl;
+                    return_val = DEPTS_ERROR_CLICK_PARSE;
+                }
+                else if (error == click::InterfaceError::CallError)
+                {
+                    std::cerr << "Error calling click command" << std::endl;
+                    return_val = DEPTS_ERROR_CLICK_CALL;
+                }
+                else
+                {
+                    std::cerr << "An unknown click error occured" << std::endl;
+                    return_val = DEPTS_ERROR_CLICK_UNKNOWN;
+                }
+            }
             qt_ready.set_value();
+        });
+        });
     });
 
     thr.join();
-    thr2.join();
 
     return return_val;
 }
