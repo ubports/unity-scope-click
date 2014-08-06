683aa4be33aa56fa7eaff1060b2ac74969bfca05
Pawel Stolowski <pawel.stolowski@canonical.com>
2014-08-06 19:31:50 +0200

    Insert names for top-level departments in the update schema script.

--- data/update_schema.sh
+++ data/update_schema.sh
@@ -23,6 +23,19 @@ then
     # updating deptnames may fail if en_US is already there, in such case just delete entries for empty locale
     sqlite3 "$DBFILE" "UPDATE deptnames SET locale='en_US' WHERE locale=''" || sqlite3 "$DBFILE" "DELETE FROM deptnames WHERE locale=''"
     sqlite3 "$DBFILE" << _UPDATE_TO_VER2
+
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('universal-access','en_US','Universal Access');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('office','en_US','Office');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('graphics','en_US','Graphics');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('science-engineering','en_US','Science & Engineering');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('internet','en_US','Internet');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('accessories','en_US','Accessories');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('developer-tools','en_US','Developer Tools');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('games','en_US','Games');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('books-magazines','en_US','Books & Magazines');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('education','en_US','Education');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('sound-video','en_US','Sound & Video');
+
     BEGIN TRANSACTION;
     DROP VIEW depts_v;
     INSERT INTO depts (deptid,parentid) VALUES ('universal-access','');
