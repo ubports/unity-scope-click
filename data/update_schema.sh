d646dcdef37e4fec25ee5e37b34338e710a372e0
Pawel Stolowski <pawel.stolowski@canonical.com>
2014-08-18 16:13:21 +0200

    Added update from schema ver 2 to 3.

--- data/update_schema.sh
+++ data/update_schema.sh
@@ -52,4 +52,100 @@ then
     UPDATE meta SET value='2' WHERE name='version';
     END TRANSACTION;
 _UPDATE_TO_VER2
+SCHEMA_VERSION=$(sqlite3 "$DBFILE" "SELECT value FROM meta WHERE name='version'")
+fi
+
+if [ "x$SCHEMA_VERSION" = "x2" ]
+then
+    sqlite3 "$DBFILE" << _UPDATE_TO_VER3
+    BEGIN TRANSACTION;
+    UPDATE pkgmap SET deptid='developer-tools' WHERE deptid='3d';
+    UPDATE pkgmap SET deptid='developer-tools' WHERE deptid='computing-robotics';
+    UPDATE pkgmap SET deptid='developer-tools' WHERE deptid='debugging';
+    UPDATE pkgmap SET deptid='developer-tools' WHERE deptid='electronics';
+    UPDATE pkgmap SET deptid='developer-tools' WHERE deptid='engineering';
+    UPDATE pkgmap SET deptid='developer-tools' WHERE deptid='ides';
+    UPDATE pkgmap SET deptid='developer-tools' WHERE deptid='profiling';
+    UPDATE pkgmap SET deptid='developer-tools' WHERE deptid='version-control';
+    UPDATE pkgmap SET deptid='developer-tools' WHERE deptid='web-development';
+    UPDATE pkgmap SET deptid='science-engineering' WHERE deptid='astronomy';
+    UPDATE pkgmap SET deptid='science-engineering' WHERE deptid='biology';
+    UPDATE pkgmap SET deptid='science-engineering' WHERE deptid='chemistry';
+    UPDATE pkgmap SET deptid='science-engineering' WHERE deptid='geography';
+    UPDATE pkgmap SET deptid='science-engineering' WHERE deptid='geology';
+    UPDATE pkgmap SET deptid='science-engineering' WHERE deptid='mathematics';
+    UPDATE pkgmap SET deptid='science-engineering' WHERE deptid='medicine';
+    UPDATE pkgmap SET deptid='science-engineering' WHERE deptid='physics';
+    UPDATE pkgmap SET deptid='games' WHERE deptid='board-games';
+    UPDATE pkgmap SET deptid='games' WHERE deptid='card-games';
+    UPDATE pkgmap SET deptid='games' WHERE deptid='puzzles';
+    UPDATE pkgmap SET deptid='games' WHERE deptid='role-playing';
+    UPDATE pkgmap SET deptid='books-comics' WHERE deptid='books-magazines';
+    UPDATE pkgmap SET deptid='communication' WHERE deptid='chat';
+    UPDATE pkgmap SET deptid='communication' WHERE deptid='file-sharing';
+    UPDATE pkgmap SET deptid='communication' WHERE deptid='internet';
+    UPDATE pkgmap SET deptid='communication' WHERE deptid='mail';
+    UPDATE pkgmap SET deptid='communication' WHERE deptid='web-browsers';
+    UPDATE pkgmap SET deptid='graphics' WHERE deptid='drawing';
+    UPDATE pkgmap SET deptid='graphics' WHERE deptid='graphic-interface-design';
+    UPDATE pkgmap SET deptid='graphics' WHERE deptid='photography';
+    UPDATE pkgmap SET deptid='graphics' WHERE deptid='publishing';
+    UPDATE pkgmap SET deptid='graphics' WHERE deptid='scanning-ocr';
+    UPDATE pkgmap SET deptid='personalisation' WHERE deptid='localization';
+    UPDATE pkgmap SET deptid='personalisation' WHERE deptid='themes-tweaks';
+    UPDATE pkgmap SET deptid='productivity' WHERE deptid='office';
+    UPDATE pkgmap SET deptid='music-audio' WHERE deptid='sound-video';
+    UPDATE pkgmap SET deptid='universal-accessaccessibility' WHERE deptid='universal-access';
+    UPDATE pkgmap SET deptid='accessories' WHERE deptid='viewers';
+
+    DELETE FROM depts;
+    INSERT INTO depts (deptid,parentid) VALUES ('travel-local', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('productivity', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('finance', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('universal-accessaccessibility', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('entertainment', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('communication', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('graphics', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('science-engineering', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('news-magazines', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('accessories', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('sports', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('developer-tools', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('games', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('books-comics', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('music-audio', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('shopping', '');
+    INSERT INTO depts (deptid,parentid) VALUES ('education', '');
+
+    DELETE FROM deptnames;
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Books & Comics','en_US','books-comics');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Business','en_US','business');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Communication','en_US','communication');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Developer Tools','en_US','developer-tools');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Education','en_US','education');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Entertainment','en_US','entertainment');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Finance','en_US','finance');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Food & Drink','en_US','food-drink');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Games','en_US','games');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Graphics','en_US','graphics');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Lifestyle','en_US','health-fitness');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Health & Fitness','en_US','lifestyle');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Media & Video','en_US','media-video');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Medical','en_US','medical');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Music & Audio','en_US','music-audio');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('News & Magazines','en_US','news-magazines');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Personalisation','en_US','personalisation');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Productivity','en_US','productivity');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Reference','en_US','reference');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Science & Engineering','en_US','science-engineering');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Shopping','en_US','shopping');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Social Networking','en_US','social-networking');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Sports','en_US','sports');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Travel & Local','en_US','travel-local');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Universal Access/Accessibility','en_US','universal-accessaccessibility');
+    INSERT INTO deptnames (deptid,locale,name) VALUES ('Weather','en_US','weather');
+
+    UPDATE meta SET value='3' WHERE name='version';
+    END TRANSACTION;
+_UPDATE_TO_VER3
 fi
