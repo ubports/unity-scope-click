e1a98ee2677ea9fa33b0a6486ca66bcbc37d8b38
Rodney Dawes <rodney.dawes@canonical.com>
2014-09-30 23:36:54 -0400

    Add methods to peek at response headers in the Response object.

--- libclickscope/click/webclient.h
+++ libclickscope/click/webclient.h
@@ -78,6 +78,9 @@ public:
     virtual void abort();
     virtual ~Response();
 
+    virtual bool has_header(const std::string& header);
+    virtual std::string get_header(const std::string& header);
+
 public slots:
     void replyFinished();
     void errorHandler(QNetworkReply::NetworkError network_error);
