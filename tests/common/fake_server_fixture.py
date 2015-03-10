aaeb32179d04c2027e61f5137737cd2dda793742
Alejandro J. Cura <alecu@canonical.com>
2015-03-10 19:32:41 -0300

    Factor out server fixture

---
+++ tests/common/fake_server_fixture.py
@@ -0,0 +1,51 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Copyright (C) 2013, 2014, 2015 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Base class for fake server fixtures."""
+
+
+import logging
+import threading
+
+import fixtures
+
+logger = logging.getLogger(__name__)
+
+
+class FakeServerFixture(fixtures.Fixture):
+
+    def __init__(self, server_class):
+        super(FakeServerFixture, self).__init__()
+        self.server_class = server_class
+
+    def setUp(self):
+        super(FakeServerFixture, self).setUp()
+        self._start_fake_server()
+
+    def _start_fake_server(self):
+        logger.info('Starting fake server: {}.'.format(self.server_class))
+        server_address = ('', 0)
+        fake_server = self.server_class(server_address)
+        server_thread = threading.Thread(target=fake_server.serve_forever)
+        server_thread.start()
+        logger.info('Serving at port {}.'.format(fake_server.server_port))
+        self.addCleanup(self._stop_fake_server, server_thread, fake_server)
+        self.url = 'http://localhost:{}/'.format(fake_server.server_port)
+
+    def _stop_fake_server(self, thread, server):
+        logger.info('Stopping fake server: {}.'.format(self.server_class))
+        server.shutdown()
+        thread.join()
