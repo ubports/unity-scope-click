5acc19c5529d13bd64ba827d7f0674f6ec3a4b0c
Antti Kaijanmäki <antti.kaijanmaki@canonical.com>
2016-03-03 14:19:50 +0000

    Add test.

--- libclickscope/tests/test_interface.cpp
+++ libclickscope/tests/test_interface.cpp
@@ -365,6 +365,20 @@ TEST(ClickInterface, testFindAppByKeywordCaseInsensitive)
     EXPECT_EQ(1, results.size());
 }
 
+TEST(ClickInterface, testFindAppAccended)
+{
+    QSharedPointer<click::KeyFileLocator> keyFileLocator(
+                new click::KeyFileLocator(
+                    testing::systemApplicationsDirectoryForTesting(),
+                    testing::userApplicationsDirectoryForTesting()));
+
+    click::Interface iface(keyFileLocator);
+
+    auto results = iface.find_installed_apps("Camara");
+
+    EXPECT_EQ(1, results.size());
+}
+
 TEST(ClickInterface, testIsIconIdentifier)
 {
     EXPECT_TRUE(Interface::is_icon_identifier("contacts-app"));
