a618d8791b308eb17c2b3e92211268b07e70f9af
Rodney Dawes <rodney.dawes@canonical.com>
2016-06-29 15:40:39 -0400

    Use shortened appid for click app launching url.

--- libclickscope/click/interface.cpp
+++ libclickscope/click/interface.cpp
@@ -212,6 +212,7 @@ click::Application Interface::load_app_from_desktop(const unity::util::IniParser
         if (id.length() == 3) {
             app.name = id[0].toUtf8().data();
             app.version = id[2].toUtf8().data();
+            app.url = "application:///" + id[0].toStdString() + "_" + id[1].toStdString();
         } else {
             app.name = "unknown";
             app.version = "unknown";
